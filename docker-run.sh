a7e6364dd5f80ee399765f0ba722f44748e48acd
Simon Kaltgrad <simonkalt@gmail.com>
2026-03-31 15:41:27 -0700

    added new template.

--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,8 @@
 docker rm -f cc_mobile_backend 2>/dev/null || true
 docker run -it \
-
   --cpus="4" \
   --cpu-shares="512" \
---memory="8g" \
+  --memory="8g" \
   -p 8675:8675 \
   --env-file .env \
   --name cc_mobile_backend \
